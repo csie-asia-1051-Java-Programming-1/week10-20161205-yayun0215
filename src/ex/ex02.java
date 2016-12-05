0850ea4774aef93f0beb85b511eace91a8fbf4dd
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-12-05 12:41:55 +0800

    week10 init

---
+++ src/ex/ex02.java
@@ -0,0 +1,15 @@
+﻿package ex;
+/*
+ * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
+ * Date: 2016/12/05
+ * Author: 1050210XX 周永振老師
+ */
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ex/ex03.java
@@ -0,0 +1,15 @@
+﻿package ex;
+/*
+ * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
+ * Date: 2016/12/05
+ * Author: 1050210XX 周永振老師
+ */
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
